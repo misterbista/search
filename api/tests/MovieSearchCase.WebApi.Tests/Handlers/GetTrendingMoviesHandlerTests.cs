1fa4aa4c64a91777f340b48a638bd85ef56decf7
Piyushraj Bista <piyushrajbista@outlook.com>
2026-09-16 10:13:43 +0545

    test: cover movie search handler

--- api/tests/MovieSearchCase.WebApi.Tests/Handlers/GetTrendingMoviesHandlerTests.cs
+++ api/tests/MovieSearchCase.WebApi.Tests/Handlers/GetTrendingMoviesHandlerTests.cs
@@ -7,6 +7,7 @@ using MovieSearchCase.Domain.Interfaces.Services;
 using MovieSearchCase.WebApi.Handlers.Movies;
 using Xunit;
 using ApiMovie = MovieSearchCase.WebApi.Models.Movies.Movie;
+using ApiMovieSearchResult = MovieSearchCase.WebApi.Models.Movies.MovieSearchResult;
 
 namespace MovieSearchCase.WebApi.Tests.Handlers;
 
@@ -38,5 +39,42 @@ public class GetTrendingMoviesHandlerTests
         var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
         var movies = okResult.Value.Should().BeAssignableTo<IEnumerable<ApiMovie>>().Subject;
         movies.Should().ContainSingle(movie => movie.Id == 1 && movie.Title == "A Trending Movie");
+    }
+
+    [Fact]
+    public async Task SearchHandleShouldReturnMappedPagedResultsWhenServiceSucceeds()
+    {
+        var searchResult = new MovieSearchResult
+        {
+            Movies =
+            [
+                new Movie
+                {
+                    Id = 1,
+                    Title = "A Search Result",
+                    VoteAverage = 8.1,
+                },
+            ],
+            Page = 2,
+            TotalPages = 3,
+            TotalResults = 51,
+        };
+
+        var movieServiceMock = new Mock<IMovieService>();
+        movieServiceMock
+            .Setup(service => service.SearchAsync("search", 2, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(searchResult);
+
+        var handler = new SearchMoviesHandler(movieServiceMock.Object, "search", 2);
+        var httpContext = new DefaultHttpContext();
+
+        var result = await handler.HandleAsync(httpContext.Request);
+
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        var response = okResult.Value.Should().BeOfType<ApiMovieSearchResult>().Subject;
+        response.Page.Should().Be(2);
+        response.TotalPages.Should().Be(3);
+        response.TotalResults.Should().Be(51);
+        response.Movies.Should().ContainSingle(movie => movie.Id == 1 && movie.Title == "A Search Result");
     }
 }
